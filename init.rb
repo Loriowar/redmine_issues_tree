b2971e16920d589f34867b6666f1232df570dd04
Ivan Zabrovskiy <loriowar@gmail.com>
2017-06-05 11:40:11 +0300

    Make a tree view as a default index page (#8)
    - implement plugin setting for switch default issues#index between plain and tree view; logic based on referer attribute of request and this may be incompatible with some security plugins for browser; alternative variant: use explicit params into all links on default issues#index page, but this leads to broken redmine hook conception and to other problems
    - add en and ru locales and add stubs for other locales

--- init.rb
+++ init.rb
@@ -9,10 +9,12 @@ Redmine::Plugin.register plugin_name do
   version RedmineIssuesTree::VERSION
   url 'https://github.com/Loriowar/redmine_issues_tree'
   author_url 'https://github.com/Loriowar'
+
+  settings partial: 'settings/redmine_issues_tree', default: { 'default_redirect_to_tree_view' => false }
 end
 
 Rails.configuration.to_prepare do
-  require_patch plugin_name, %w(issue issues_helper)
+  require_patch plugin_name, %w(issue issues_helper issues_controller)
 end
 
 # Assign permissions on a tree_view actions. Permissions is same as for :view_issues.
