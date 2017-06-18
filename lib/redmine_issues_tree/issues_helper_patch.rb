37e35bc9b15b2baadd90feb8bbdd75414814d57f
Ivan Zabrovskiy <loriowar@gmail.com>
2017-06-18 12:53:50 +0300

    Tree view for all issues#index without project scope (#30)
    - on issues#index page without project scope implemented ability to switch to a tree view

--- lib/redmine_issues_tree/issues_helper_patch.rb
+++ lib/redmine_issues_tree/issues_helper_patch.rb
@@ -7,10 +7,12 @@ module RedmineIssuesTree::IssuesHelperPatch
         link_to l(:tree_view, scope: 'issues_tree'), '#',
                 class: 'icon icon-orange-tree issues-tree-view-link',
                 data: {link_to_tree_view: redirect_with_params_project_issues_trees_path(project_id: project)}
+      else
+        link_to l(:tree_view, scope: 'issues_tree'), '#',
+                class: 'icon icon-orange-tree issues-tree-view-link',
+                data: {link_to_tree_view: redirect_with_params_issues_trees_path}
       end
     end
   end
 
 end
-
-
