39d6d8299eefcd38398f8502762889891cc5c7ef
Xiaoli Wang <nsnq@qq.com>
2018-05-10 13:11:01 +0800

    enable default treeview on top Issues page

--- lib/redmine_issues_tree/issues_controller_patch.rb
+++ lib/redmine_issues_tree/issues_controller_patch.rb
@@ -9,12 +9,16 @@ module RedmineIssuesTree::IssuesControllerPatch
     skip_issues_tree_redirect = params.delete(:skip_issues_tree_redirect)
 
     # @note: do not change behaviour of issues#index for all projects
-    if Setting.plugin_redmine_issues_tree.with_indifferent_access[:default_redirect_to_tree_view] == 'true' && params[:project_id].present?
-      # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
-      if skip_issues_tree_redirect == 'true' || URI(request.referer).path == project_issues_path
-        index_without_redmine_issues_tree
+    if Setting.plugin_redmine_issues_tree.with_indifferent_access[:default_redirect_to_tree_view] == 'true' 
+      if params[:project_id].present?
+        # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
+        if skip_issues_tree_redirect == 'true' || URI(request.referer).path == project_issues_path
+          index_without_redmine_issues_tree
+        else
+          redirect_to tree_index_project_issues_trees_path(request.query_parameters)
+        end
       else
-        redirect_to tree_index_project_issues_trees_path(request.query_parameters)
+        redirect_to tree_index_issues_trees_path(request.query_parameters)
       end
     else
       index_without_redmine_issues_tree
