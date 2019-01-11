3af455cb189b12bc08022983cba53ce670f61047
Ivan Zabrovskiy <loriowar@gmail.com>
2019-01-11 12:36:22 +0300

    Fix update of feed in case of default redirect to tree view
    - prevent any plugin logic on case of non-http request to issues#index (thanks to @micjahn see #85)

--- lib/redmine_issues_tree/issues_controller_patch.rb
+++ lib/redmine_issues_tree/issues_controller_patch.rb
@@ -1,31 +1,35 @@
 module RedmineIssuesTree::IssuesControllerPatch
   def index
-    skip_issues_tree_redirect = params.delete(:skip_issues_tree_redirect)
+    if request.format.html?
+      skip_issues_tree_redirect = params.delete(:skip_issues_tree_redirect)
 
-    if params[:project_id].present?
-      if Setting.plugin_redmine_issues_tree.with_indifferent_access[:default_redirect_to_tree_view] == 'true'
-        # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
-        # @reason: we must prevent redirect to the tree_view if we do some actions on a plain view
-        if skip_issues_tree_redirect == 'true' || URI(request.referer).path == project_issues_path
-          super
+      if params[:project_id].present?
+        if Setting.plugin_redmine_issues_tree.with_indifferent_access[:default_redirect_to_tree_view] == 'true'
+          # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
+          # @reason: we must prevent redirect to the tree_view if we do some actions on a plain view
+          if skip_issues_tree_redirect == 'true' || URI(request.referer).path == project_issues_path
+            super
+          else
+            redirect_to tree_index_project_issues_trees_path(request.query_parameters)
+          end
         else
-          redirect_to tree_index_project_issues_trees_path(request.query_parameters)
+          super
         end
       else
-        super
+        if Setting.plugin_redmine_issues_tree.with_indifferent_access[:default_redirect_to_tree_view_without_project] == 'true'
+          # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
+          # @reason: we must prevent redirect to the tree_view if we do some actions on a plain view
+          if skip_issues_tree_redirect == 'true' || URI(request.referer).path == issues_path
+            super
+          else
+            redirect_to tree_index_issues_trees_path(request.query_parameters)
+          end
+        else
+          super
+        end
       end
     else
-      if Setting.plugin_redmine_issues_tree.with_indifferent_access[:default_redirect_to_tree_view_without_project] == 'true'
-        # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
-        # @reason: we must prevent redirect to the tree_view if we do some actions on a plain view
-        if skip_issues_tree_redirect == 'true' || URI(request.referer).path == issues_path
-          super
-        else
-          redirect_to tree_index_issues_trees_path(request.query_parameters)
-        end
-      else
-        super
-      end
+      super
     end
   end
 
