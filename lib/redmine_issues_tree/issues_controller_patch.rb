e3aae418386b2e6351b29091ff7ef9d58b2faea5
Pablo Luque Salguero <45895450+pluque01@users.noreply.github.com>
2026-05-07 18:20:00 +0200

    fix: check if referer is nil before parsing
    Add check for referer presence before comparing paths.

--- lib/redmine_issues_tree/issues_controller_patch.rb
+++ lib/redmine_issues_tree/issues_controller_patch.rb
@@ -9,7 +9,8 @@ module RedmineIssuesTree
           if settings[:default_redirect_to_tree_view] == 'true'
             # @note: add additional parameter into all links on issues#index looks not so good as parsing a referer
             # @reason: we must prevent redirect to the tree_view if we do some actions on a plain view
-            if skip_issues_tree_redirect == 'true' || URI(request.referer).path == project_issues_path
+            if skip_issues_tree_redirect == 'true' ||
+              (request.referer && URI(request.referer).path == project_issues_path)
               super
             else
               redirect_to tree_index_project_issues_trees_path(request.query_parameters)
